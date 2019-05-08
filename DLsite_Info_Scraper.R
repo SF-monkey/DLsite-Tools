ecbeb0fd1e9f9eb1fa1d13d05da947b3d3d41200
SaferSky <6609835+SaferSky@users.noreply.github.com>
2019-05-07 23:26:39 -0700

    Update DLsite_Info_Scraper.R

--- DLsite_Info_Scraper.R
+++ DLsite_Info_Scraper.R
@@ -4,7 +4,7 @@
 #
 #How To Use This Program:
 #1.Get the base url by DLsite keyword search (showtype must be 1), with just the number of page left
-#eg: https://www.dlsite.com/maniax/fsr/=/language/jp/sex_category%5B0%5D/male/keyword/陽向葵ゅか/order%5B0%5D/release_d/per_page/30/show_type/1/page/
+#eg: https://www.dlsite.com/maniax/circle/profile/=/language/jp/sex_category%5B0%5D/male/keyword_maker_name/RG17643/order%5B0%5D/release_d/per_page/30/show_type/1/hd/1/page/
 #2.Look for total number of pages by yourself, so you don't go over that
 #3.Now run all the code once
 #4.Then, type: "get_info('paste_the_base_url_here', and_#pages_here)"
