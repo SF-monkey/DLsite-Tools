b0f0a4e5c3c00c9d59568f794b78a8b2e8920cb1
SaferSky <6609835+SaferSky@users.noreply.github.com>
2019-05-07 23:16:20 -0700

    Add files via upload

---
+++ DLsite_match.R
@@ -0,0 +1,24 @@
+##########################################################################################################
+#Purpose of this function:
+#To look up the products missed from the full product list.
+#You MUST use the csv generated from "DLsite_Info_Scraper.R"
+#to run this program!!!
+
+#Assume you downloaded some games/manga of a certain series from DLsite
+#and all files are properly named. eg: "[Date][RJXXXXXX][maker] title"
+#By using DLsite_Info_Scraper.R to get all product info of that series,
+#you can now find out what games/manga you've missed from the list.
+##########################################################################################################
+
+get_missed = function(csv, filepath){
+  
+  full_list = read.csv(csv, header=T) #Get the full list of product info
+  file = list.files(filepath) #Get the file list under a folder
+  
+  #Get only the product IDs (eg: RJXXXXXX)
+  file = gsub("(?:.*)\\[(?:[^\\]]+)\\]\\[([^\\]]+)\\]\\[(?:[^\\]]+)\\](?:.*)", "\\1", file, perl=T)
+  
+  #compare and store
+  missed = setdiff(full_list[,2], file)
+  write.csv(missed, file="missed.csv", row.names=F)
+}
